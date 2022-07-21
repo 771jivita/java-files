8d400b84dc0619fe81f99ffb80075756e4f0a836
Jivita Sharma <jivitasharma771@gmail.com>
2022-07-21 15:58:32 +0530

    commit
    this is a simple interest program using java

---
+++ Compare.java
@@ -0,0 +1,11 @@
+class Compare{
+public static void main(String args[]){
+int a=100;
+if(a<100){
+System.out.println("Less then 100");
+}
+else{
+System.out.println("error");
+}
+}
+}
---
+++ Simple_interest.java
@@ -0,0 +1,9 @@
+class Simple_interest{
+public static void main(String args[]){
+int principle,rate,time,simple_i;
+principle=10;
+rate=20;
+time=30;
+simple_i= principle*rate*time/100;
+System.out.println(simple_i);
+}}
