21de404c89e0e81c83e70100b7b684e90b664e72
Jivita Sharma <109589154+771jivita@users.noreply.github.com>
2022-07-23 02:01:41 +0530

    Add files via upload
    Java program on size of data types

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+   /* boolean data type
+    byte data type
+    char data type
+    short data type
+    int data type
+    long data type
+    float data type
+    double data type */
+public class DataTypes{
+public static void main(String args[]){
+byte b = 1;
+System.out.println("size of byte is:"+ Byte.BYTES);
+char c ='j';
+System.out.println("size of character is:"+ Character.BYTES); 
+short s=2;
+System.out.println("size of short is:"+ Short.BYTES); 
+int a = 1;
+System.out.println("size of integer is:"+ Integer.BYTES);
+long l = 2222;
+System.out.println("size of long is:"+ Long.BYTES); 
+float ff = 1.1f;
+System.out.println("size of float is:"+ Float.BYTES);
+double d= 11.2;
+System.out.println("size of double is:"+ Double.BYTES); 
+
+}}
